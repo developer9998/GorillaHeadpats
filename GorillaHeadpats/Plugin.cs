18bbfcae4942d5c735bcde15bdc651aae09a84fe
elliot! <benjikrolfanpageuwu@gmail.com>
2025-07-14 12:26:06 +0100

    Update Plugin.cs

--- GorillaHeadpats/Plugin.cs
+++ GorillaHeadpats/Plugin.cs
@@ -11,16 +11,18 @@ namespace GorillaHeadpats
         public static ConfigEntry<float> HapticAmplitude;
 
         public static ConfigEntry<bool> UseCatSounds;
+        public static ConfigEntry<bool> UseRaccoonSounds;
 
         public void Awake()
         {
             PetVolume = Config.Bind(Constants.Name, "Pet Volume", 0.28f, new ConfigDescription("The volume of the pet sound", new AcceptableValueRange<float>(0.05f, 0.5f)));
             HapticAmplitude = Config.Bind(Constants.Name, "Haptic Amplitude", 0.25f, new ConfigDescription("The strength of the haptic made when petting", new AcceptableValueRange<float>(0f, 1f)));
-            
+
             UseCatSounds = Config.Bind(Constants.Name, "Use Cat Toy Sounds", false, "Whether cat toy squeek sounds should replace the regular petting");
-            
+            UseRaccoonSounds = Config.Bind(Constants.Name, "Use Raccoon Toy Sounds", false, "Whether raccoon toy squeek sounds should replace the regular petting");
+
             Harmony.CreateAndPatchAll(typeof(Plugin).Assembly, Constants.GUID);
         }
     }
 }
-//meow
+//meow and raccoon i guess..
