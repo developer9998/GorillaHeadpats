a824297ad2d5e73ed6de0b86f1e3d9465fafb3f4
elliot! <benjikrolfanpageuwu@gmail.com>
2025-07-15 15:33:35 +0100

    sponge

--- GorillaHeadpats/Behaviours/PetCollider.cs
+++ GorillaHeadpats/Behaviours/PetCollider.cs
@@ -16,8 +16,24 @@ namespace GorillaHeadpats.Behaviours
             if (collider.TryGetComponent(out GorillaTriggerColliderHandIndicator component) && !activated)
             {
                 activated = true;
-                await PetCat(component.isLeftHand);
-                await Pet(component.isLeftHand);
+                bool isLeft = component.isLeftHand;
+
+                if (Plugin.UseRaccoonSounds.Value)
+                {
+                    await PetRaccoon(isLeft);
+                }
+                else if (Plugin.UseCatSounds.Value)
+                {
+                    await PetCat(isLeft);
+                }
+                else if (Plugin.UseSpongeSounds.Value)
+                {
+                    await PetSponge(isLeft);
+                }
+                else
+                {
+                    Player.PlaySoundCat(EPatSound.Default, isLeft);
+                }
                 activated = false;
             }
         }
@@ -37,7 +53,7 @@ namespace GorillaHeadpats.Behaviours
                 Player.PlaySoundCat(EPatSound.CatRelease, isLeftHand);
         }
 
-        public async Task Pet(bool isLeftHand)
+        public async Task PetRaccoon(bool isLeftHand)
         {
             float amplitude = Mathf.Clamp(Plugin.HapticAmplitude.Value, 0f, 1f);
             if (!Mathf.Approximately(amplitude, 0f))
@@ -50,6 +66,21 @@ namespace GorillaHeadpats.Behaviours
 
             if (isRacoon)
                 Player.PlaySoundRaccoon(EPatSound.RaccoonRelease, isLeftHand);
+        }
+
+        public async Task PetSponge(bool isLeftHand)
+        {
+            float amplitude = Mathf.Clamp(Plugin.HapticAmplitude.Value, 0f, 1f);
+            if (!Mathf.Approximately(amplitude, 0f))
+                GorillaTagger.Instance.StartVibration(isLeftHand, amplitude, GorillaTagger.Instance.tapHapticDuration);
+
+            bool isSponge = Plugin.UseSpongeSounds.Value;
+            Player.PlaySoundSponge(isSponge ? EPatSound.SpongeSqueeze : EPatSound.Default, isLeftHand);
+
+            await Task.Delay(125);
+
+            if (isSponge)
+                Player.PlaySoundSponge(EPatSound.SpongeRelease, isLeftHand);
         }
     }
 }
