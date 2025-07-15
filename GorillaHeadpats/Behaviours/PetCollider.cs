ab62cea3087b819e2ca2b3bd1bd4304e6a1c0075
elliot! <benjikrolfanpageuwu@gmail.com>
2025-07-15 16:17:59 +0100

    Update PetCollider.cs

--- GorillaHeadpats/Behaviours/PetCollider.cs
+++ GorillaHeadpats/Behaviours/PetCollider.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using UnityEngine;
 using GorillaHeadpats.Models;
@@ -18,70 +17,39 @@ namespace GorillaHeadpats.Behaviours
                 activated = true;
                 bool isLeft = component.isLeftHand;
 
-                if (Plugin.UseRaccoonSounds.Value)
+                switch (Plugin.SelectedPetType.Value)
                 {
-                    await PetRaccoon(isLeft);
+                    case PetType.Raccoon:
+                        await Pet(isLeft, EPatSound.RaccoonSqueeze, EPatSound.RaccoonRelease);
+                        break;
+                    case PetType.Cat:
+                        await Pet(isLeft, EPatSound.CatSqueeze, EPatSound.CatRelease);
+                        break;
+                    case PetType.Sponge:
+                        await Pet(isLeft, EPatSound.SpongeSqueeze, EPatSound.SpongeRelease);
+                        break;
+                    default:
+                        await Pet(isLeft, EPatSound.Default);
+                        break;
                 }
-                else if (Plugin.UseCatSounds.Value)
-                {
-                    await PetCat(isLeft);
-                }
-                else if (Plugin.UseSpongeSounds.Value)
-                {
-                    await PetSponge(isLeft);
-                }
-                else
-                {
-                    Player.PlaySoundCat(EPatSound.Default, isLeft);
-                }
+
                 activated = false;
             }
         }
 
-        public async Task PetCat(bool isLeftHand)
+        private async Task Pet(bool isLeftHand, EPatSound contactSound, EPatSound? releaseSound = null)
         {
             float amplitude = Mathf.Clamp(Plugin.HapticAmplitude.Value, 0f, 1f);
             if (!Mathf.Approximately(amplitude, 0f))
                 GorillaTagger.Instance.StartVibration(isLeftHand, amplitude, GorillaTagger.Instance.tapHapticDuration);
 
-            bool isCat = Plugin.UseCatSounds.Value;
-            Player.PlaySoundCat(isCat ? EPatSound.CatSqueeze : EPatSound.Default, isLeftHand);
+            Player.PlayPetSound(contactSound, isLeftHand);
 
-            await Task.Delay(125);
-
-            if (isCat)
-                Player.PlaySoundCat(EPatSound.CatRelease, isLeftHand);
-        }
-
-        public async Task PetRaccoon(bool isLeftHand)
-        {
-            float amplitude = Mathf.Clamp(Plugin.HapticAmplitude.Value, 0f, 1f);
-            if (!Mathf.Approximately(amplitude, 0f))
-                GorillaTagger.Instance.StartVibration(isLeftHand, amplitude, GorillaTagger.Instance.tapHapticDuration);
-
-            bool isRacoon = Plugin.UseRaccoonSounds.Value;
-            Player.PlaySoundRaccoon(isRacoon ? EPatSound.RaccoonSqueeze : EPatSound.Default, isLeftHand);
-
-            await Task.Delay(125);
-
-            if (isRacoon)
-                Player.PlaySoundRaccoon(EPatSound.RaccoonRelease, isLeftHand);
-        }
-
-        public async Task PetSponge(bool isLeftHand)
-        {
-            float amplitude = Mathf.Clamp(Plugin.HapticAmplitude.Value, 0f, 1f);
-            if (!Mathf.Approximately(amplitude, 0f))
-                GorillaTagger.Instance.StartVibration(isLeftHand, amplitude, GorillaTagger.Instance.tapHapticDuration);
-
-            bool isSponge = Plugin.UseSpongeSounds.Value;
-            Player.PlaySoundSponge(isSponge ? EPatSound.SpongeSqueeze : EPatSound.Default, isLeftHand);
-
-            await Task.Delay(125);
-
-            if (isSponge)
-                Player.PlaySoundSponge(EPatSound.SpongeRelease, isLeftHand);
+            if (releaseSound.HasValue)
+            {
+                await Task.Delay(125);
+                Player.PlayPetSound(releaseSound.Value, isLeftHand);
+            }
         }
     }
 }
-
