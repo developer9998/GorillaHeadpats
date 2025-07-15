a2318a63f5c704e18c9232abf0997cbc9c464c8a
elliot! <benjikrolfanpageuwu@gmail.com>
2025-07-15 16:17:01 +0100

    Update Plugin.cs

--- GorillaHeadpats/Plugin.cs
+++ GorillaHeadpats/Plugin.cs
@@ -4,27 +4,34 @@ using HarmonyLib;
 
 namespace GorillaHeadpats
 {
+    public enum PetType
+    {
+        Default,
+        Raccoon,
+        Cat,
+        Sponge,
+    }
+
     [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
     public class Plugin : BaseUnityPlugin
     {
         public static ConfigEntry<float> PetVolume;
         public static ConfigEntry<float> HapticAmplitude;
 
-        public static ConfigEntry<bool> UseCatSounds;
-        public static ConfigEntry<bool> UseRaccoonSounds;
-        public static ConfigEntry<bool> UseSpongeSounds;
+        public static ConfigEntry<PetType> SelectedPetType;
 
         public void Awake()
         {
-            PetVolume = Config.Bind(Constants.Name, "Pet Volume", 0.28f, new ConfigDescription("The volume of the pet sound", new AcceptableValueRange<float>(0.05f, 0.5f)));
-            HapticAmplitude = Config.Bind(Constants.Name, "Haptic Amplitude", 0.25f, new ConfigDescription("The strength of the haptic made when petting", new AcceptableValueRange<float>(0f, 1f)));
+            PetVolume = Config.Bind(Constants.Name, "Pet Volume", 0.28f,
+                new ConfigDescription("The volume of the pet sound", new AcceptableValueRange<float>(0.05f, 0.5f)));
 
-            UseCatSounds = Config.Bind(Constants.Name, "Use Cat Toy Sounds", false, "Whether cat toy squeek sounds should replace the regular petting");
-            UseRaccoonSounds = Config.Bind(Constants.Name, "Use Raccoon Toy Sounds", false, "Whether raccoon toy squeek sounds should replace the regular petting");
-            UseSpongeSounds = Config.Bind(Constants.Name, "Use Sponge Sounds", false, "Whether sponge sounds should replace the regular petting");
+            HapticAmplitude = Config.Bind(Constants.Name, "Haptic Amplitude", 0.25f,
+                new ConfigDescription("The strength of the haptic made when petting", new AcceptableValueRange<float>(0f, 1f)));
+
+            SelectedPetType = Config.Bind(Constants.Name, "Pet Sound Type", PetType.Default,
+                "The type of pet sound to play");
 
             Harmony.CreateAndPatchAll(typeof(Plugin).Assembly, Constants.GUID);
         }
     }
 }
-//meow and raccoon i guess..
